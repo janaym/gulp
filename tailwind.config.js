dd768784afee5bc067f5e5977aed920446891c17
Janay Ma <janay.ma7@gmail.com>
2023-12-13 11:22:50 -0800

    Setup Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue, js, jsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
