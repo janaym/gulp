cc3aa5276f763a62772e5d7a58a1ee8d59466906
Janay Ma <janay.ma7@gmail.com>
2023-12-13 14:47:28 -0800

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue, js, jsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': '#E5EbF8',
+        'ash-blue': '#586EA2',
+        'navy': '#003975',
+        'dark-navy': '001033',
+      },
+    },
   },
   plugins: [],
 }
